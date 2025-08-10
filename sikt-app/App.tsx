14285543a10cba97bfd8359a9633c0917282da2e
simaking <47402478+simaking@users.noreply.github.com>
2025-08-10 22:49:07 +0200

    Endret navn.

--- sikt-app/App.tsx
+++ sikt-app/App.tsx
@@ -59,7 +59,7 @@ export default function App() {
 
         {/* Purple Student Profile Card */}
         <View style={styles.profileCard}>
-          <Text style={styles.nameText}>Even Martin Ab elseth Riksheim (23)</Text>
+          <Text style={styles.nameText}>Simon Ishoel (23)</Text>
           <View style={styles.infoRow}>
             <MaterialIcons name="calendar-today" size={18} color="#1a1a1a" />
             <Text style={styles.infoText}>Fødselsdato: 24.09.2001</Text>
